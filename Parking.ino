fb89b56d9479216d01f1c6ffb83b57477570d869
Dhananjay Narayan <37698599+DhananjayNarayan@users.noreply.github.com>
2018-03-24 11:43:53 +0530

    Parking Code Complete

--- Parking.ino
+++ Parking.ino
@@ -32,3 +32,48 @@ void setup()
     
   }
 }
+void loop() {  
+   int sensorval1 = digitalRead(sensor1); 
+   int sensorval2 = digitalRead(sensor2);
+   int sensorval3 = digitalRead(sensor3);
+   Serial.println(sensorval1);             
+   Serial.println(sensorval2);
+   Serial.println(sensorval3);
+   delay(1000);
+   if (sensorval1 == 1)
+   {
+led1.on();  
+client.sendAll(true);
+}
+   if (sensorval2 == 1)
+   {
+led2.on();
+client.sendAll(true);
+}
+   if (sensorval3 == 1)
+   {
+
+led3.on();
+client.sendAll(true);
+}
+    if (sensorval1 == 0)
+   {
+
+led1.off();
+client.sendAll(false);
+
+}
+   if (sensorval2 == 0)
+   {
+led2.off();
+client.sendAll(false);
+}
+   if (sensorval3 == 0)
+   {
+led3.off();
+client.sendAll(false);
+}   
+    
+    Blynk.run();
+    
+     }
